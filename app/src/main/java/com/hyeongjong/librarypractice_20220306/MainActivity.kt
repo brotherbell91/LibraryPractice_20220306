9e0e03fb213af45e5e3d6573204ab5cbeac86ff8
이형종 <hj36879@naver.com>
2022-03-06 11:42:03 +0900

    실제 권한 확인 기능 실행

--- app/src/main/java/com/hyeongjong/librarypractice_20220306/MainActivity.kt
+++ app/src/main/java/com/hyeongjong/librarypractice_20220306/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.hyeongjong.librarypractice_20220306
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 //            권한이 있는지 확인하고 진행.
 //            확인 ==> 획특 / 거부인지 상황에 따라 다른 행동.
 
-            val pl = object  : PermissionListener {
+            val pl = object : PermissionListener {
 
                 override fun onPermissionGranted() {
 //                    권한이 획득 되었을 때 할 행동 적는 함수
@@ -31,19 +33,24 @@ class MainActivity : AppCompatActivity() {
 //                    임시 : CALL 기능 실제 활용 => 앱이 죽을 예정
 
                     val myUri = Uri.parse("tel:010-5555-5555")
-                    val myIntent = Intent( Intent.ACTION_CALL, myUri)
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
                     startActivity(myIntent)
-
-                }
-
                 }
 
                 override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
 //                    최종 권한 거부 되었을 떄 할 행동
-                    Toast.makeText(this@MainActivity, "권한이 거부되어 전화 연결이 불가능합니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "권한이 거부되어 전화 연결이 불가능합니다.", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
             }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
         imgProfile.setOnClickListener {
 
